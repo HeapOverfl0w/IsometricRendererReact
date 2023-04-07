2f3e3cf506561721d8a0255b4c728d772708cbfd
blufx44 <blufx44@vt.edu>
2023-04-07 11:07:58 -0400

    increased cell count

--- src/components/map-grid.js
+++ src/components/map-grid.js
@@ -59,12 +59,12 @@ export default function MapGrid(props) {
   return (
     <div style={{ display: 'block', position: 'absolute', width: 'inherit', height: 'inherit' }}>
       <div style={props.visible ? { display: 'block', position: 'relative', width: 'inherit', height: 'inherit', overflow: 'hidden' } : { display: 'none' }}>
-        <Grid container spacing={0} columns={{ xs: 1 }} style={{ 
+        <Grid container spacing={0} columns={{ xs: 100 }} style={{ 
           width: window.innerWidth + 'px', 
           height: window.innerHeight + 'px', 
           transform: `rotate(24.8deg) skewX(319.5deg) translateX(${((CAMERA_ZOOM + props.zoom)-(props.center.x ? props.center.x/TILE_WIDTH : CAMERA_X))*4*(scale)*(window.innerWidth/100)+TILE_HEIGHT}px) translateY(${((props.center.y ? props.center.y/TILE_HEIGHT : CAMERA_Y)-(CAMERA_ZOOM + props.zoom))*4*(scale)*(window.innerHeight/100)-TILE_WIDTH}px) scale(${scale})`}}>
           {
-            Array.from(Array(100)).map((_, index) => (
+            Array.from(Array(10000)).map((_, index) => (
               <Grid xs={1} item key={index}>
                 <div className={valid ? classes.valid : classes.invalid} onMouseEnter={() => handleEnter()}></div>
               </Grid>
